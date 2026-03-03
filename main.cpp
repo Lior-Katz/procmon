6ab30957425e82741cacd760b4bb6fd8c3000899
Lior Katz <liorkatz21@gmail.com>
2026-03-03 14:48:24 +0200

    add slnx

--- main.cpp
+++ main.cpp
@@ -70,12 +70,12 @@ void EventRecordCallback(PEVENT_RECORD pEvent)
 	// 	if (status != ERROR_SUCCESS){
 	// 		std::cout << "TdhGetEventInformation initial call failed with status " << status << std::endl;
 	// 	}
-		
+
 	// 	PWSTR eventName = (PWSTR)((PBYTE)pInfo + pInfo->EventNameOffset);
 	// 	wprintf(L"Received event: %s\n", eventName);
 	// 	// Further parsing of properties would go here, often using TdhFormatProperty
 	// 	// or directly accessing the property data based on the pInfo structure.
-		
+
 	// }
 	// else if (status != ERROR_SUCCESS)
 	// {
@@ -85,12 +85,12 @@ void EventRecordCallback(PEVENT_RECORD pEvent)
 	unsigned long pid = pEvent->EventHeader.ProcessId;
 	auto event_id = pEvent->EventHeader.EventDescriptor.Id;
 
-	if (event_id != PROCESS_CREATED_EVENT_ID  && event_id != PROCESS_EXITED_EVENT_ID)
+	if (event_id != PROCESS_CREATED_EVENT_ID && event_id != PROCESS_EXITED_EVENT_ID)
 	{
 		return;
 	}
-	
-	std::ostream *output = static_cast<std::ostream *>(pEvent->UserContext);
+
+	std::ostream* output = static_cast<std::ostream*>(pEvent->UserContext);
 	std::string msg = "Event id: " + std::to_string(event_id) + "; pid: " + std::to_string(pid);
 	Trace(*output, msg);
 }
@@ -98,6 +98,8 @@ void EventRecordCallback(PEVENT_RECORD pEvent)
 void CreateTraceSession(wchar_t* session_name)
 {
 	TracePropsWithName trace;
+	CONTROLTRACE_ID trace_id = 0;
+
 	//EVENT_TRACE_PROPERTIES props = trace.props;
 	ULONG propsSize = sizeof(trace.props);
 
@@ -106,11 +108,12 @@ void CreateTraceSession(wchar_t* session_name)
 
 	trace.props.Wnode.BufferSize = bufferSize;
 	trace.props.Wnode.Flags = WNODE_FLAG_TRACED_GUID;
+
 	trace.props.LogFileMode = EVENT_TRACE_REAL_TIME_MODE;
 	trace.props.LoggerNameOffset = offsetof(TracePropsWithName, sessionName);;
 	wcscpy_s(trace.sessionName, session_name);
 
-	auto res = StartTraceW(NULL, session_name, &trace.props);
+	auto res = StartTraceW(&trace_id, session_name, &trace.props);
 	if (res != ERROR_SUCCESS)
 	{
 		cout << "Got error code " << res << " from StartTraceW" << endl;
@@ -118,16 +121,16 @@ void CreateTraceSession(wchar_t* session_name)
 	}
 }
 
-PROCESSTRACE_HANDLE OpenTraceSession(wchar_t* session_name, void *context)
+PROCESSTRACE_HANDLE OpenTraceSession(wchar_t* session_name, void* context)
 {
-	PEVENT_TRACE_LOGFILE Logfile;
+	EVENT_TRACE_LOGFILE Logfile;
 	ZeroMemory(&Logfile, sizeof(Logfile));
-	Logfile->LogFileName = nullptr;
-	Logfile->LoggerName = session_name;
-	Logfile->ProcessTraceMode = PROCESS_TRACE_MODE_EVENT_RECORD | PROCESS_TRACE_MODE_REAL_TIME;
-	Logfile->EventRecordCallback = EventRecordCallback;
-	Logfile->Context = context;
-	auto trace_handle = OpenTraceW(Logfile);
+	Logfile.LogFileName = nullptr;
+	Logfile.LoggerName = session_name;
+	Logfile.ProcessTraceMode = PROCESS_TRACE_MODE_EVENT_RECORD | PROCESS_TRACE_MODE_REAL_TIME;
+	Logfile.EventRecordCallback = EventRecordCallback;
+	Logfile.Context = context;
+	auto trace_handle = OpenTraceW(&Logfile);
 	if (trace_handle == INVALID_PROCESSTRACE_HANDLE)
 	{
 		throw runtime_error("Failed opening trace session");
